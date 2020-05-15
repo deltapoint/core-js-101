f54f998297c555f2d260e3c2db8edef410937a35
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-05-15 21:44:28 +0300

    feat: implement some 'fs-n-closures-tasks' funcs

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -62,8 +62,10 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (!args.length) return null;
+  return (x) => args
+    .reduce((acc, v, i) => acc + v * (x ** (arguments.length - i - 1)), 0);
 }
 
 
@@ -102,8 +104,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return (...args) => {
+    for (let i = 0; i <= attempts;) {
+      try {
+        return func.apply(args);
+      } catch (e) {
+        i += 1;
+      }
+    }
+    return 'error';
+  };
 }
 
 
@@ -130,8 +141,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = `${func.name}(${JSON.stringify(Array.from(args)).slice(1, -1)})`;
+    logFunc(`${argsStr} starts`);
+    const result = func(...args);
+    logFunc(`${argsStr} ends`);
+    return result;
+  };
 }
 
 
@@ -148,8 +165,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn(...(args1.concat(args)));
 }
 
 
@@ -170,8 +187,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+  return () => {
+    count += 1;
+    return count;
+  };
 }
 
 
